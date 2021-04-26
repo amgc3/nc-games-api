707721f1fe9e66a69286fb913420b27fa81b3746
Johann Hannerth <hannerth94@gmail.com>
2021-04-26 13:01:08 +0100

    adds requires for seed.js

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,4 +1,8 @@
-exports.seed = function () {
+const format = require("pg-format");
+const db = require("../connection.js");
+const { dropTables, createTables } = require("../manage-tables.js");
+
+exports.seed = function ({ categoryData, commentData, reviewData, userData }) {
   // add seeding functionality here
   // this function should take as argument(s) all the data it needs to seed
   // it should insert this data into the relevant tables in your database
