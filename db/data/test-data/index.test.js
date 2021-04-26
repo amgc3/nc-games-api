929f5896f299505e95a3e3bd96082cd414c6581a
Johann Hannerth <hannerth94@gmail.com>
2021-04-26 11:44:05 +0100

    adds index files exporting both dev and test data

---
+++ db/data/test-data/index.test.js
@@ -0,0 +1,4 @@
+exports.categoriesData = require("./categories.js");
+exports.commentsData = require("./comments.js");
+exports.reviewsData = require("./reviews.js");
+exports.usersData = require("./users.js");
