e2a1abfde31093fc53a68c9524dcb76059fe8516
Anna <adropofwater0@gmail.com>
2021-04-28 16:39:12 +0100

    reviews router

--- routers/review.router.js
+++ routers/review.router.js
@@ -1,6 +1,7 @@
-const express = require("express");
+const express = require('express');
+const { getReviews } = require('../controllers/review.controller');
 const reviewRouter = express.Router();
 
-reviewRouter.route("/").get(getReviews);
+reviewRouter.route('/').get(getReviews);
 
 module.exports = reviewRouter;
