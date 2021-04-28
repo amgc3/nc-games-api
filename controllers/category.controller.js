5d92e617734f9f584bcd0792b48921c69af1cb5b
Anna <adropofwater0@gmail.com>
2021-04-28 16:40:22 +0100

    removed console.log from getCategories

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,12 +1,11 @@
-const { selectCategories } = require("../models/categories.model.js");
+const { selectCategories } = require('../models/categories.model.js');
 
 exports.getCategories = (req, res) => {
   selectCategories()
     .then((categories) => {
-      console.log(categories);
       res.status(200).send({ categories });
     })
     .catch((err) => {
-      res.status(500).send({ msg: "internal server error" });
+      res.status(500).send({ msg: 'internal server error' });
     });
 };
