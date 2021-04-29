ca7423387162c653835a688cc1174fe5ce47ac03
Anna <adropofwater0@gmail.com>
2021-04-29 16:15:49 +0100

    sql queries file

--- sql_files/test.sql
+++ sql_files/test.sql
@@ -1,3 +1,6 @@
 \c nc_games_test
 
-SELECT * FROM comments;
+SELECT reviews.* , COUNT(comments.review_id) AS comments_count FROM reviews 
+LEFT JOIN comments on reviews.review_id = comments.review_id
+WHERE reviews.review_id = 2
+GROUP BY reviews.review_id;
