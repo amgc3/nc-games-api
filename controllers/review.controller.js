5909cc163b0f9f3a8709da71d9477ef2806aeb88
Anna <adropofwater0@gmail.com>
2021-04-29 12:56:35 +0100

    Internal Server Error

--- controllers/review.controller.js
+++ controllers/review.controller.js
@@ -24,6 +24,8 @@ exports.getReview = (req, res) => {
         res.status(400).send({ msg: 'Bad Request' });
       } else if (err.status && err.msg) {
         res.status(err.status).send({ msg: err.msg });
+      } else {
+        res.status(500).send({ msg: 'Internal Server Error' });
       }
     });
 };
