6bfa03384849a820a24efc3e4cd14c15ca0c1ea0
Anna <adropofwater0@gmail.com>
2021-04-29 12:51:38 +0100

    changed variable name

--- routers/review.router.js
+++ routers/review.router.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { getReviews } = require('../controllers/review.controller');
+const { getReviews, getReview } = require('../controllers/review.controller');
 const reviewRouter = express.Router();
 
 reviewRouter.route('/').get(getReviews);
+reviewRouter.route('/:review_id').get(getReview);
 
 module.exports = reviewRouter;
