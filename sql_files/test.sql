632d9bd13d444ef4c36ddd448922e0eb12bd7c08
Anna <adropofwater0@gmail.com>
2021-04-30 15:40:24 +0100

    add reviews query

--- sql_files/test.sql
+++ sql_files/test.sql
@@ -4,3 +4,8 @@ SELECT reviews.* , COUNT(comments.review_id) AS comments_count FROM reviews
 LEFT JOIN comments on reviews.review_id = comments.review_id
 WHERE reviews.review_id = 2
 GROUP BY reviews.review_id;
+
+SELECT reviews.* , COUNT(comments.review_id) 
+  AS comments_count FROM reviews 
+  JOIN comments ON reviews.review_id = comments.review_id
+  GROUP BY reviews.review_id;
