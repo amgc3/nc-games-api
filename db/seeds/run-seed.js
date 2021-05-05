019428b825b3a7b4bee4cd98203e72feea2fbb53
Anna <adropofwater0@gmail.com>
2021-05-05 12:42:26 +0100

    create run-seed for development data

--- db/seeds/run-seed.js
+++ db/seeds/run-seed.js
@@ -3,9 +3,14 @@ Here you will need to require in:
 - the seed function, 
 - the dev data,
 */
+const db = require('../connection')
+const { seed } = require('./seed');
+const data = require('../data/development-data');
 
-const runSeed = async () => {
+
+const runSeed =  () => {
   // run the seed with the data
+  return seed(data).then(() => db.end())
 };
 
 runSeed();
